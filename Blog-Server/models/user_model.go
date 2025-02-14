5aa700fd0c2999acdccdd50ef09d4b63743306c0
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-14 20:53:29 +0800

    feat-表结构搭建-用户配置表

--- Blog-Server/models/user_model.go
+++ Blog-Server/models/user_model.go
@@ -1,6 +1,8 @@
 // models/user_model.go
 package models
 
+import "time"
+
 type UserModel struct {
 	Model
 	Username       string `gorm:"size:32" json:"username"`
@@ -13,4 +15,15 @@ type UserModel struct {
 	Email          string `gorm:"size:256" json:"email"`
 	OpenID         string `gorm:"size:64" json:"openID"` // 第三方登陆的唯一id
 
+}
+
+type UserConfModel struct {
+	UserID             uint       `gorm:"unique" json:"userID"`
+	UserModel          UserModel  `gorm:"foreignKey:UserID" json:"-"`
+	LikeTags           []string   `gorm:"type:longtext;serializer:json" json:"likeTags"`
+	UpdateUsernameDate *time.Time `json:"updateUsernameDate"` // 上次修改用户名的时间
+	OpenCollect        bool       `json:"openCollect"`        // 公开我的收藏
+	OpenFans           bool       `json:"openFans"`           // 公开我的粉丝
+	OpenFollow         bool       `json:"openFollow"`         // 公开我的关注
+	HomeStyleID        uint       `json:"homeStyleID"`        //主页样式
 }
