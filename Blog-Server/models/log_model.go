90de94b0b75ba31b39ab2429fce6ddefdace8780
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-15 09:10:54 +0800

    feat-表结构-用户登录表

--- Blog-Server/models/log_model.go
+++ Blog-Server/models/log_model.go
@@ -2,12 +2,12 @@ package models
 
 type LogModel struct {
 	LogType   int8      `json:"logType"` // 日志类型
-	Title     string    `json:"title"`
+	Title     string    `gorm:"size:64" json:"title"`
 	Content   string    `json:"content"`
 	Level     int8      `json:"level"`  // 日志级别
 	UserID    uint      `json:"userID"` // 用户id
 	UserModel UserModel `gorm:"foreignKey:UserID" json:"-"`
-	IP        string    `json:"ip"`
-	Addr      string    `json:"addr"`
+	IP        string    `gorm:"size:32" json:"ip"`
+	Addr      string    `gorm:"size:64" json:"addr"`
 	IsRead    bool      `json:"isRead"` // 是否已读
 }
