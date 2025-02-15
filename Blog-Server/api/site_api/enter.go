151a47e0630f415b75a242e567a33592f695c571
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-15 12:09:05 +0800

    test-日志-登录日志测试，apifox创建项目

--- Blog-Server/api/site_api/enter.go
+++ Blog-Server/api/site_api/enter.go
@@ -1,6 +1,8 @@
 package site_api
 
 import (
+	"Blog-Server/models/enum"
+	"Blog-Server/service/log_service"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +12,8 @@ type SiteApi struct {
 
 func (SiteApi) SiteInfoView(c *gin.Context) {
 	fmt.Println("1")
+	log_service.NewLoginSuccess(c, enum.UserPwdLoginType)
+	log_service.NewLoginFail(c, enum.UserPwdLoginType, "用户不存在", "lingyuan", "1234")
 	c.JSON(200, gin.H{"code": 0, "msg": "站点信息"})
 	return
 }
