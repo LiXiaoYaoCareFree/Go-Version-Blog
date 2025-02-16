96d61c39dbde8ee748d987a5230bfce006e51822
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-16 17:09:02 +0800

    BMS-admin框架搭建-html部分

--- backstage-management-system/src/views/admin/index.vue
+++ backstage-management-system/src/views/admin/index.vue
@@ -3,9 +3,39 @@
 </script>
 
 <template>
-<div>admin</div>
+<div class="CodeForge_admin">
+  <div class="CodeForge_aside">
+    <div class="CodeForge_logo">
+
+    </div>
+    <div class="CodeForge_menu">
+
+    </div>
+  </div>
+  <div class="CodeForge_main">
+    <div class="CodeForge_head">
+      <div class="CodeForge_breadcrumbs"></div>
+      <div class="CodeForge_actions">
+          <icon-home />
+          <icon-sun-fill />
+          <icon-moon-fill />
+          <icon-fullscreen-exit />
+          <div class="CodeForge_user_info_action">
+
+          </div>
+      </div>
+    </div>
+    <div class="CodeForge_tabs">
+
+
+    </div>
+    <div class="CodeForge_container">
+      <router-view></router-view>
+    </div>
+  </div>
+</div>
 </template>
 
-<style scoped>
+<style lang="less">
 
 </style>
