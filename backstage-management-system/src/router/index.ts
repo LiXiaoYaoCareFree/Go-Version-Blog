a56f52e32a7205b6f93da18e095c73db5a7a8bad
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-16 14:36:42 +0800

    BMS-路由搭建

--- backstage-management-system/src/router/index.ts
+++ backstage-management-system/src/router/index.ts
@@ -4,7 +4,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      name: "web",
+      path: "/",
+      // component: () => import("@/views/web/index.vue"),
+      redirect: "/admin",
+    },
 
+    {
+      name: "login",
+      path: "/login",
+      component: () => import("@/views/login/index.vue"),
+    },
+
+    {
+      name: "admin",
+      path: "/admin",
+      component: () => import("@/views/admin/index.vue"),
+    },
   ]
 })
 
