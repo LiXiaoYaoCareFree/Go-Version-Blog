4a02cb4f04cc02e67a840d5b6ac8ae15b315a8d3
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-19 19:42:08 +0800

    feat-gin静态路由和模式配置

--- Blog-Server/router/enter.go
+++ Blog-Server/router/enter.go
@@ -7,8 +7,11 @@ import (
 )
 
 func Run() {
+	gin.SetMode(global.Config.System.GinMode)
+
 	r := gin.Default()
 
+	r.Static("/uploads", "uploads")
 	nr := r.Group("/api")
 
 	nr.Use(middleware.LogMiddleware)
