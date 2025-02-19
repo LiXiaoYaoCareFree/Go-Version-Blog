3ecb70fbafd43377244f0b99fe8bb5c877e2e058
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-19 22:07:30 +0800

    feat-日志管理-日志列表

--- Blog-Server/api/enter.go
+++ Blog-Server/api/enter.go
@@ -1,9 +1,13 @@
 package api
 
-import "Blog-Server/api/site_api"
+import (
+	"Blog-Server/api/log_api"
+	"Blog-Server/api/site_api"
+)
 
 type Api struct {
 	SiteApi site_api.SiteApi
+	LogApi  log_api.LogApi
 }
 
 var App = Api{}
