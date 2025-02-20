ecb147a764c283bf47073dcaef76c774cca0bede
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-20 11:09:03 +0800

    feat-站点配置-类型定义

---
+++ Blog-Server/conf/conf_ai.go
@@ -0,0 +1,9 @@
+// conf/conf_ai.go
+package conf
+
+type Ai struct {
+	Enable    bool   `yaml:"enable" json:"enable"`
+	SecretKey string `yaml:"secretKey" json:"secretKey"`
+	Nickname  string `yaml:"nickname" json:"nickname"`
+	Avatar    string `yaml:"avatar" json:"avatar"`
+}
