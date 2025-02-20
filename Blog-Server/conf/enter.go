04741da21da2e3624bbf3ba5fd0332ef9d73b2d7
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-20 11:09:03 +0800

    feat-站点配置-类型定义

--- Blog-Server/conf/enter.go
+++ Blog-Server/conf/enter.go
@@ -7,4 +7,9 @@ type Config struct {
 	DB     DB     `yaml:"db"`  //读库
 	DB1    DB     `yaml:"db1"` //写库
 	Redis  Redis  `yaml:"redis"`
+	Site   Site   `yaml:"site"`
+	Email  Email  `yaml:"email"`
+	QQ     QQ     `yaml:"qq"`
+	QiNiu  QiNiu  `yaml:"qiNiu"`
+	Ai     Ai     `yaml:"ai"`
 }
