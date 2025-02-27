4616b826347f50d2ba5823ac597ca7e27b0ea121
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-28 00:08:30 +0800

    feat-文件管理-图片上传

--- Blog-Server/api/enter.go
+++ Blog-Server/api/enter.go
@@ -1,13 +1,15 @@
 package api
 
 import (
+	"Blog-Server/api/image_api"
 	"Blog-Server/api/log_api"
 	"Blog-Server/api/site_api"
 )
 
 type Api struct {
-	SiteApi site_api.SiteApi
-	LogApi  log_api.LogApi
+	SiteApi  site_api.SiteApi
+	LogApi   log_api.LogApi
+	ImageApi image_api.ImageApi
 }
 
 var App = Api{}
