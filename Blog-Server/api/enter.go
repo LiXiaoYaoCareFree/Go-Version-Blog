9ddd3f08160e20cfe09ffa2ec717b754a0f5201b
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-28 19:52:15 +0800

    feat-图片验证码

--- Blog-Server/api/enter.go
+++ Blog-Server/api/enter.go
@@ -2,16 +2,18 @@ package api
 
 import (
 	"Blog-Server/api/banner_api"
+	"Blog-Server/api/captcha_api"
 	"Blog-Server/api/image_api"
 	"Blog-Server/api/log_api"
 	"Blog-Server/api/site_api"
 )
 
 type Api struct {
-	SiteApi   site_api.SiteApi
-	LogApi    log_api.LogApi
-	ImageApi  image_api.ImageApi
-	BannerApi banner_api.BannerApi
+	SiteApi    site_api.SiteApi
+	LogApi     log_api.LogApi
+	ImageApi   image_api.ImageApi
+	BannerApi  banner_api.BannerApi
+	CaptchaApi captcha_api.CaptchaApi
 }
 
 var App = Api{}
