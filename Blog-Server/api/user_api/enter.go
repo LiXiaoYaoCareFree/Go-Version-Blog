ab70067f2e10c7a564b3f7eeb1336fef3a24c903
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-28 22:54:30 +0800

    feat-给邮箱发邮件

---
+++ Blog-Server/api/user_api/enter.go
@@ -0,0 +1,5 @@
+// api/user_api/enter.go
+package user_api
+
+type UserApi struct {
+}
---
+++ Blog-Server/api/user_api/send_email.go
@@ -0,0 +1,54 @@
+// api/user_api/send_email.go
+package user_api
+
+import (
+	"Blog-Server/common/res"
+	"Blog-Server/global"
+	"Blog-Server/models"
+	"Blog-Server/service/email_service"
+	"github.com/gin-gonic/gin"
+	"github.com/mojocn/base64Captcha"
+	"github.com/sirupsen/logrus"
+)
+
+type SendEmailRequest struct {
+	Type  int8   `json:"type" binding:"oneof=1 2"` // 1 注册 2 重置密码
+	Email string `json:"email" binding:"required"`
+}
+type SendEmailResponse struct {
+	EmailID string `json:"emailID"`
+}
+
+func (UserApi) SendEmailView(c *gin.Context) {
+	var cr SendEmailRequest
+	err := c.ShouldBindJSON(&cr)
+	if err != nil {
+		res.FailWithError(err, c)
+		return
+	}
+	code := base64Captcha.RandText(4, "0123456789")
+	id := base64Captcha.RandomId()
+
+	switch cr.Type {
+	case 1:
+		// 查邮箱是否不存在
+		var user models.UserModel
+		err = global.DB.Take(&user, "email = ?", cr.Email).Error
+		if err == nil {
+			res.FailWithMsg("该邮箱以使用", c)
+			return
+		}
+		err = email_service.SendRegisterCode(cr.Email, code)
+	case 2:
+		err = email_service.SendResetPwdCode(cr.Email, code)
+	}
+	if err != nil {
+		logrus.Errorf("邮件发送失败 %s", err)
+		res.FailWithMsg("邮件发送失败", c)
+		return
+	}
+	global.CaptchaStore.Set(id, code)
+	res.OkWithData(SendEmailResponse{
+		EmailID: id,
+	}, c)
+}
