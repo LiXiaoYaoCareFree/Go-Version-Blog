5b2cc2b540f6e5b3e053c25b8cbb6913a6a177fc
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-02 20:40:08 +0800

    feat-用户信息修改

--- Blog-Server/router/user_router.go
+++ Blog-Server/router/user_router.go
@@ -19,4 +19,5 @@ func UserRouter(r *gin.RouterGroup) {
 	r.PUT("user/password", middleware.AuthMiddleware, app.UpdatePasswordView)
 	r.PUT("user/password/reset", middleware.EmailVerifyMiddleware, app.ResetPasswordView)
 	r.PUT("user/email/bind", middleware.EmailVerifyMiddleware, middleware.AuthMiddleware, app.BindEmailView)
+	r.PUT("user", middleware.AuthMiddleware, app.UserInfoUpdateView)
 }
