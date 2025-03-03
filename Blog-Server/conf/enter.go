2f302ba52364e1ea021ae0f64565f93593a6296e
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-03 17:52:08 +0800

    perf-mysql主从配置优化

--- Blog-Server/conf/enter.go
+++ Blog-Server/conf/enter.go
@@ -4,8 +4,7 @@ type Config struct {
 	System System `yaml:"system"`
 	Log    Log    `yaml:"log"`
 	Jwt    Jwt    `yaml:"jwt"`
-	DB     DB     `yaml:"db"`  //读库
-	DB1    DB     `yaml:"db1"` //写库
+	DB     []DB   `yaml:"db"` // 数据库连接列表
 	Redis  Redis  `yaml:"redis"`
 	Site   Site   `yaml:"site"`
 	Email  Email  `yaml:"email"`
