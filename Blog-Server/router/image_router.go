1a414e6943624795fa21489d5a24cbaf296f5b3d
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-04 14:38:46 +0800

    feat-图片转存功能

--- Blog-Server/router/image_router.go
+++ Blog-Server/router/image_router.go
@@ -3,6 +3,7 @@ package router
 
 import (
 	"Blog-Server/api"
+	"Blog-Server/api/image_api"
 	"Blog-Server/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,7 @@ func ImageRouter(r *gin.RouterGroup) {
 	app := api.App.ImageApi
 	r.POST("images", middleware.AuthMiddleware, app.ImageUploadView)
 	r.POST("images/qiniu", middleware.AuthMiddleware, app.QiNiuGenToken)
+	r.POST("images/transfer_deposit", middleware.AuthMiddleware, middleware.BindJsonMiddleware[image_api.TransferDepositRequest], app.TransferDepositView)
 	r.GET("images", middleware.AdminMiddleware, app.ImageListView)
 	r.DELETE("images", middleware.AdminMiddleware, app.ImageRemoveView)
 }
