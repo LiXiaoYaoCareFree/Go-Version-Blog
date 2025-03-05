49e2d3a06c0086f4fc55e4a1895b850d4dd96dad
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-05 23:55:25 +0800

    feat-评论树接口

--- Blog-Server/router/comment_router.go
+++ Blog-Server/router/comment_router.go
@@ -5,10 +5,12 @@ import (
 	"Blog-Server/api"
 	"Blog-Server/api/comment_api"
 	"Blog-Server/middleware"
+	"Blog-Server/models"
 	"github.com/gin-gonic/gin"
 )
 
 func CommentRouter(r *gin.RouterGroup) {
 	app := api.App.CommentApi
 	r.POST("comment", middleware.AuthMiddleware, middleware.BindJsonMiddleware[comment_api.CommentCreateRequest], app.CommentCreateView)
+	r.GET("comment/tree/:id", middleware.BindUriMiddleware[models.IDRequest], app.CommentTreeView)
 }
