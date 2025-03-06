bf74db5699b129b8268d63fbba8c3ea94583fdc2
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-06 22:18:10 +0800

    feat-站内信列表接口

--- Blog-Server/router/site_msg_router.go
+++ Blog-Server/router/site_msg_router.go
@@ -12,4 +12,5 @@ func SiteMsgRouter(r *gin.RouterGroup) {
 	app := api.App.SiteMsgApi
 	r.GET("site_msg/conf", middleware.AuthMiddleware, app.UserSiteMessageConfView)
 	r.PUT("site_msg/conf", middleware.AuthMiddleware, middleware.BindJsonMiddleware[site_msg_api.UserMessageConfUpdateRequest], app.UserSiteMessageConfUpdateView)
+	r.GET("site_msg", middleware.AuthMiddleware, middleware.BindQueryMiddleware[site_msg_api.SiteMsgListRequest], app.SiteMsgListView)
 }
