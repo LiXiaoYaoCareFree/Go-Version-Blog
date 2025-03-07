117fa72e7d8edf16ed50fea7dfc9a700e48bbbd0
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-07 20:03:10 +0800

    feat-好友关系-批量好友关系查询

--- Blog-Server/testdata/25.好友关系.go
+++ Blog-Server/testdata/25.好友关系.go
@@ -4,7 +4,7 @@ import (
 	"Blog-Server/core"
 	"Blog-Server/flags"
 	"Blog-Server/global"
-	"Blog-Server/models"
+	"Blog-Server/service/focus_service"
 	"fmt"
 )
 
@@ -14,10 +14,15 @@ func main() {
 	core.InitLogrus()
 	global.DB = core.InitDB()
 
-	fmt.Println(models.CalcUserRelationship(1, 2))
-	fmt.Println(models.CalcUserRelationship(1, 3))
-	fmt.Println(models.CalcUserRelationship(4, 1))
-	fmt.Println(models.CalcUserRelationship(5, 1))
-	fmt.Println(models.CalcUserRelationship(1, 4))
+	fmt.Println(focus_service.CalcUserRelationship(1, 2))
+	fmt.Println(focus_service.CalcUserRelationship(1, 3))
+	fmt.Println(focus_service.CalcUserRelationship(4, 1))
+	fmt.Println(focus_service.CalcUserRelationship(5, 1))
+	fmt.Println(focus_service.CalcUserRelationship(3, 4))
+	fmt.Println(focus_service.CalcUserPatchRelationship(1, []uint{2, 3, 4, 5})) // 2：4  3：2   4：2  5:3
+	fmt.Println(focus_service.CalcUserPatchRelationship(2, []uint{1, 3, 4, 5}))
+	fmt.Println(focus_service.CalcUserPatchRelationship(3, []uint{1, 2, 4, 5}))
+	fmt.Println(focus_service.CalcUserPatchRelationship(4, []uint{1, 2, 3, 5}))
+	fmt.Println(focus_service.CalcUserPatchRelationship(5, []uint{1, 2, 3, 4}))
 
 }
