b813a15b5c5d08645bb3e7139d393c3e4b4d88b1
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-07 21:12:13 +0800

    feat-私信-用户对话方法

--- Blog-Server/testdata/25.好友关系.go
+++ Blog-Server/testdata/25.好友关系.go
@@ -4,8 +4,7 @@ import (
 	"Blog-Server/core"
 	"Blog-Server/flags"
 	"Blog-Server/global"
-	"Blog-Server/service/focus_service"
-	"fmt"
+	"Blog-Server/service/chat_service"
 )
 
 func main() {
@@ -14,15 +13,18 @@ func main() {
 	core.InitLogrus()
 	global.DB = core.InitDB()
 
-	fmt.Println(focus_service.CalcUserRelationship(1, 2))
-	fmt.Println(focus_service.CalcUserRelationship(1, 3))
-	fmt.Println(focus_service.CalcUserRelationship(4, 1))
-	fmt.Println(focus_service.CalcUserRelationship(5, 1))
-	fmt.Println(focus_service.CalcUserRelationship(3, 4))
-	fmt.Println(focus_service.CalcUserPatchRelationship(1, []uint{2, 3, 4, 5})) // 2：4  3：2   4：2  5:3
-	fmt.Println(focus_service.CalcUserPatchRelationship(2, []uint{1, 3, 4, 5}))
-	fmt.Println(focus_service.CalcUserPatchRelationship(3, []uint{1, 2, 4, 5}))
-	fmt.Println(focus_service.CalcUserPatchRelationship(4, []uint{1, 2, 3, 5}))
-	fmt.Println(focus_service.CalcUserPatchRelationship(5, []uint{1, 2, 3, 4}))
+	//fmt.Println(focus_service.CalcUserRelationship(1, 2))
+	//fmt.Println(focus_service.CalcUserRelationship(1, 3))
+	//fmt.Println(focus_service.CalcUserRelationship(4, 1))
+	//fmt.Println(focus_service.CalcUserRelationship(5, 1))
+	//fmt.Println(focus_service.CalcUserRelationship(3, 4))
+	//fmt.Println(focus_service.CalcUserPatchRelationship(1, []uint{2, 3, 4, 5})) // 2：4  3：2   4：2  5:3
+	//fmt.Println(focus_service.CalcUserPatchRelationship(2, []uint{1, 3, 4, 5}))
+	//fmt.Println(focus_service.CalcUserPatchRelationship(3, []uint{1, 2, 4, 5}))
+	//fmt.Println(focus_service.CalcUserPatchRelationship(4, []uint{1, 2, 3, 5}))
+	//fmt.Println(focus_service.CalcUserPatchRelationship(5, []uint{1, 2, 3, 4}))
+
+	chat_service.ToTextChat(1, 2, "你好")
+	chat_service.ToTextChat(2, 1, "在干嘛")
 
 }
