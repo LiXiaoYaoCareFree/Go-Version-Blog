f83912b7aeeb51571e2a644d2d28ea42984cd461
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-08 14:53:48 +0800

    feat-用户删除消息

--- Blog-Server/router/chat_router.go
+++ Blog-Server/router/chat_router.go
@@ -11,4 +11,5 @@ func ChatRouter(r *gin.RouterGroup) {
 	app := api.App.ChatApi
 	r.GET("chat", middleware.AuthMiddleware, middleware.BindQueryMiddleware[chat_api.ChatListRequest], app.ChatListView)
 	r.GET("chat/session", middleware.AuthMiddleware, middleware.BindQueryMiddleware[chat_api.SessionListRequest], app.SessionListView)
+	r.DELETE("chat/:id", middleware.AuthMiddleware, middleware.BindUriMiddleware[models.IDRequest], app.UserChatDeleteView)
 }
