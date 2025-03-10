e41f8415103d722c3d4b8b3887afcfc53b69e25a
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-10 13:58:50 +0800

    服务降级

--- Blog-Server/api/ai_api/article_ai.go
+++ Blog-Server/api/ai_api/article_ai.go
@@ -1,6 +1,7 @@
 package ai_api
 
 import (
+	"Blog-Server/common"
 	"Blog-Server/common/res"
 	"Blog-Server/global"
 	"Blog-Server/middleware"
@@ -8,7 +9,6 @@ import (
 	"Blog-Server/service/ai_service"
 	"context"
 	"encoding/json"
-
 	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic/v7"
 	"github.com/sirupsen/logrus"
@@ -25,34 +25,48 @@ func (AiApi) ArticleAiView(c *gin.Context) {
 		res.SSEFail("站点未启用ai功能", c)
 		return
 	}
-	// 查这个内容关联的文章列表
-	query := elastic.NewBoolQuery()
-	query.Should(
-		elastic.NewMatchQuery("title", cr.Content),
-		elastic.NewMatchQuery("abstract", cr.Content),
-		elastic.NewMatchQuery("content", cr.Content),
-	)
-	// 只能查发布的文章
-	query.Must(elastic.NewTermQuery("status", 3))
-	result, err := global.ESClient.
-		Search(models.ArticleModel{}.Index()).
-		Query(query).
-		From(1).
-		Size(10).
-		Do(context.Background())
-	if err != nil {
-		source, _ := query.Source()
-		byteData, _ := json.Marshal(source)
-		logrus.Errorf("查询失败 %s \n %s", err, string(byteData))
-		res.SSEFail("查询失败", c)
-		return
+	var content string
+	if global.ESClient == nil {
+		// 服务降级
+		list, _, _ := common.ListQuery(models.ArticleModel{}, common.Options{
+			Likes: []string{"title", "abstract"},
+			PageInfo: common.PageInfo{
+				Page:  1,
+				Limit: 10,
+			},
+		})
+		byteData, _ := json.Marshal(list)
+		content = string(byteData)
+	} else {
+		// 查这个内容关联的文章列表
+		query := elastic.NewBoolQuery()
+		query.Should(
+			elastic.NewMatchQuery("title", cr.Content),
+			elastic.NewMatchQuery("abstract", cr.Content),
+			elastic.NewMatchQuery("content", cr.Content),
+		)
+		// 只能查发布的文章
+		query.Must(elastic.NewTermQuery("status", 3))
+		result, err := global.ESClient.
+			Search(models.ArticleModel{}.Index()).
+			Query(query).
+			From(1).
+			Size(10).
+			Do(context.Background())
+		if err != nil {
+			source, _ := query.Source()
+			byteData, _ := json.Marshal(source)
+			logrus.Errorf("查询失败 %s \n %s", err, string(byteData))
+			res.SSEFail("查询失败", c)
+			return
+		}
+		var list []string
+		for _, hit := range result.Hits.Hits {
+			list = append(list, string(hit.Source))
+		}
+		content = "[" + strings.Join(list, ",") + "]"
 	}
-	var list []string
-	for _, hit := range result.Hits.Hits {
 
-		list = append(list, string(hit.Source))
-	}
-	content := "[" + strings.Join(list, ",") + "]"
 	msgChan, err := ai_service.ChatStream(cr.Content, content)
 	if err != nil {
 		res.SSEFail("ai分析失败", c)
@@ -61,5 +75,4 @@ func (AiApi) ArticleAiView(c *gin.Context) {
 	for s := range msgChan {
 		res.SSEOk(s, c)
 	}
-
 }
