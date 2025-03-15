3292f44d12bebe0dbf546c36a5d5b1c7d4f1667b
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-15 18:09:19 +0800

    perf-数据库优化-索引优化

--- Blog-Server/api/banner_api/enter.go
+++ Blog-Server/api/banner_api/enter.go
@@ -102,6 +102,7 @@ func (BannerApi) BannerUpdateView(c *gin.Context) {
 		"cover": cr.Cover,
 		"href":  cr.Href,
 		"show":  cr.Show,
+		"type":  cr.Type,
 	}).Error
 	if err != nil {
 		res.FailWithError(err, c)
--- Blog-Server/api/site_msg_api/msg_list.go
+++ Blog-Server/api/site_msg_api/msg_list.go
@@ -7,13 +7,19 @@ import (
 	"Blog-Server/middleware"
 	"Blog-Server/models"
 	"Blog-Server/models/enum/message_type_enum"
+	"Blog-Server/models/enum/relationship_enum"
+	"Blog-Server/service/focus_service"
 	"Blog-Server/utils/jwts"
 	"github.com/gin-gonic/gin"
 )
 
 type SiteMsgListRequest struct {
 	common.PageInfo
-	T int8 `form:"t" binding:"required,oneof=1 2 3"` // 1评论和回复 2赞和收藏 3 系统
+	T int8 `form:"t" binding:"required,oneof=1 2 3"` // 1 评论和回复 2 赞和收藏 3 系统
+}
+type SiteMsgListResponse struct {
+	models.MessageModel
+	Relation relationship_enum.Relation `json:"relation"`
 }
 
 func (SiteMsgApi) SiteMsgListView(c *gin.Context) {
@@ -31,12 +37,28 @@ func (SiteMsgApi) SiteMsgListView(c *gin.Context) {
 
 	claims := jwts.GetClaims(c)
 
-	list, count, _ := common.ListQuery(models.MessageModel{
+	_list, count, _ := common.ListQuery(models.MessageModel{
 		RevUserID: claims.UserID,
 	}, common.Options{
 		PageInfo: cr.PageInfo,
 		Where:    global.DB.Where("type in ?", typeList),
 	})
-
+	var userIDList []uint
+	for _, model := range _list {
+		if model.ActionUserID != 0 {
+			userIDList = append(userIDList, model.ActionUserID)
+		}
+	}
+	var m = map[uint]relationship_enum.Relation{}
+	if len(userIDList) > 0 {
+		m = focus_service.CalcUserPatchRelationship(claims.UserID, userIDList)
+	}
+	var list = make([]SiteMsgListResponse, 0)
+	for _, model := range _list {
+		list = append(list, SiteMsgListResponse{
+			MessageModel: model,
+			Relation:     m[model.ActionUserID],
+		})
+	}
 	res.OkWithList(list, count, c)
 }
