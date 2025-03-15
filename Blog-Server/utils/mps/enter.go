cea22564dbe6ffecf0445ac502db9bca66c6c939
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-15 15:00:07 +0800

    联调-用户标签更新失败

--- Blog-Server/utils/mps/enter.go
+++ Blog-Server/utils/mps/enter.go
@@ -1,7 +1,10 @@
 // utils/mps/enter.go
 package mps
 
-import "reflect"
+import (
+	"encoding/json"
+	"reflect"
+)
 
 func StructToMap(data any, t string) (mp map[string]any) {
 	mp = make(map[string]any)
@@ -16,7 +19,13 @@ func StructToMap(data any, t string) (mp map[string]any) {
 			continue
 		}
 		if val.Kind() == reflect.Ptr {
-			mp[tag] = val.Elem().Interface()
+			v1 := val.Elem().Interface()
+			if val.Elem().Kind() == reflect.Slice {
+				byteData, _ := json.Marshal(v1)
+				mp[tag] = string(byteData)
+			} else {
+				mp[tag] = v1
+			}
 			continue
 		}
 		mp[tag] = val.Interface()
