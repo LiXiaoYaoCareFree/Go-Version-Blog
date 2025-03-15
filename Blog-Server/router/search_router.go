01bd99e59b5cb9b789cb283a54e4e0328b356db8
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-15 17:34:05 +0800

    feat-es查标签聚合列表

--- Blog-Server/router/search_router.go
+++ Blog-Server/router/search_router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"Blog-Server/api"
 	"Blog-Server/api/search_api"
+	"Blog-Server/common"
 	"Blog-Server/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +12,5 @@ func SearchRouter(r *gin.RouterGroup) {
 	app := api.App.SearchApi
 	r.GET("search/article", middleware.BindQueryMiddleware[search_api.ArticleSearchRequest], app.ArticleSearchView)
 	r.GET("search/text", middleware.BindQueryMiddleware[search_api.TextSearchRequest], app.TextSearchView)
+	r.GET("search/tags", middleware.BindQueryMiddleware[common.PageInfo], app.TagAggView)
 }
