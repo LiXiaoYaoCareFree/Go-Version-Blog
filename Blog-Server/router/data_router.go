a49b4f7d57dde910f85bec2cf2fd2498ef1d46d7
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-03-15 11:43:38 +0800

    联调-数据统计-用户统计

--- Blog-Server/router/data_router.go
+++ Blog-Server/router/data_router.go
@@ -3,6 +3,7 @@ package router
 
 import (
 	"Blog-Server/api"
+	"Blog-Server/api/data_api"
 	"Blog-Server/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +12,6 @@ func DataRouter(r *gin.RouterGroup) {
 	app := api.App.DataApi
 	r.GET("data/sum", middleware.AdminMiddleware, app.SumView)
 	r.GET("data/article", middleware.AdminMiddleware, app.ArticleDataView)
+	r.GET("data/register_user", middleware.AdminMiddleware, app.RegisterUserDataView)
+	r.GET("data/growth", middleware.AdminMiddleware, middleware.BindQueryMiddleware[data_api.GrowthDataRequest], app.GrowthDataView)
 }
